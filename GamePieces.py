b193dfd2f9745aba8b074d13c694b91697f2f2d1
Didriksson <mattias.didriksson@gmail.com>
2014-09-01 21:02:17 +0200

    Fixed Pawns again. Damn it Pawns, just do it already.

--- GamePieces.py
+++ GamePieces.py
@@ -76,46 +76,40 @@ class Pawn(GamePieces):
 	def __init__(self, image, position):
 		GamePieces.__init__(self,image, position)	
 	def pieceSpecificMove(self,source, destination, board, lightPieces, darkPieces):
+		deltaY = source[0] - destination[0]
+		deltaX = source[1] - destination[1]
 		#Light!
 		if board[source[0]][source[1]] in lightPieces.values():
-			#Destination empty
-			if board[destination[0]][destination[1]] == "EMPTY":
-				if (source[0] - destination[0]) == 1:
-					if (source[1] - destination[1] != 0):
-						return False
+			#Moves up or down on the board.
+			if (deltaY == 2 and source[0] == 6) or (deltaX == 0 and deltaY == 1):
+				if board[destination[0]][destination[1]] == "EMPTY" and deltaX == 0:
 					return True
-				elif (source[0] - destination[0]) == 2 and source[0] == 6:
-					if (source[1] - destination[1] != 0):
-						return False
-					return True
-			#Destination occupado
+				else:
+					return False
+			#Take another.
 			else:
 				print "Indeedeo"
-				if abs((source[1] - destination[1])) == 1:
+				if deltaY == 1 and (abs(deltaX) == 1):
 						if(board[destination[0]][destination[1]] in darkPieces.values()):
 							return True
 						else:
 							return False
 		#Dark!
-		elif board[source[0]][source[1]] in darkPieces.values():
-				#Destination empty
-				if board[destination[0]][destination[1]] == "EMPTY":
-					if (source[0] - destination[0]) == -1:
-						if (source[1] - destination[1] != 0):
-							return False
-						return True
-					elif (source[0] - destination[0]) == -2 and source[0] == 1:
-						if (source[1] - destination[1] != 0):
-							return False
-						return True
-				#Destination occupado
+		if board[source[0]][source[1]] in darkPieces.values():
+			#Moves up or down on the board.
+			if (deltaY == -2 and source[0] == 1) or (deltaX == 0 and deltaY == -1):
+				if board[destination[0]][destination[1]] == "EMPTY" and deltaX == 0:
+					return True
 				else:
-					if abs((source[1] - destination[1])) == 1:
-							if(board[destination[0]][destination[1]] in lightPieces.values()):
-								return True
-							else:
-								return False
-			
+					return False
+			#Take another.
+			else:
+				print "Indeedeo"
+				if deltaY == -1 and (abs(deltaX) == 1):
+						if(board[destination[0]][destination[1]] in lightPieces.values()):
+							return True
+						else:
+							return False				
 		else:
 			return False
 
